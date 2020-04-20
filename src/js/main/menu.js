a1af8e1c786cdbc4ad2a1564fb9d20019ef9a78f
777Antoniy777 <tat7106@yandex.ru>
2020-04-20 21:50:57 +0600

    fix header

--- src/js/main/menu.js
+++ src/js/main/menu.js
@@ -1,8 +1,5 @@
-// const html = document.querySelector('html');
 const body = document.querySelector('body');
-const header = body.querySelector('.main-header');
-const button = header.querySelector('.nav__button');
-const main = body.querySelector('.main');
+const button = body.querySelector('.nav__button');
 
 const onButtonClick = (evt) => {
   evt.preventDefault();
