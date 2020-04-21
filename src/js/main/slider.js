1806cf11d04f409784df5c37a0553a3aaec79857
777Antoniy777 <tat7106@yandex.ru>
2020-04-21 21:58:50 +0600

    add swiper

--- src/js/main/slider.js
+++ src/js/main/slider.js
@@ -1,5 +1,19 @@
-import swiper from 'swiper';
+import Swiper from 'swiper';
 
-const slider = document.querySelector('.programme___slider');
+console.log(Swiper)
+// const slider = document.querySelector('.programme___slider');
+
+let swiper = new Swiper('.swiper-container', {
+  speed: 400,
+  grabCursor: true,
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+});
 
 console.log(swiper);
+
+// slider = document.querySelector('.slider').swiper;
+
+// console.log(swiper);
