4923441cf8bb565e4209df4d0c5cb541a99eca63
777Antoniy777 <tat7106@yandex.ru>
2020-04-26 16:51:49 +0600

    set new slider view

--- src/js/main/slider.js
+++ src/js/main/slider.js
@@ -2,16 +2,51 @@ import { tns } from '../../../node_modules/tiny-slider/src/tiny-slider';
 
 const slider = tns({
   container: '.programme___slider',
-  items: 2,
-  slideBy: 1,
-  center: true,
-  fixedWidth: 260,
-  gutter: 10,
+  // items: 2,
+  // slideBy: 1,
+  // center: true,
+  // fixedWidth: 260,
+  // gutter: 10,
   controls: false,
   nav: false,
+
+  items: 1,
+  slideBy: 1,
+  center: true,
+  // autoWidth: true,
+  edgePadding: 15,
+  gutter: 16,
 });
 
-const nav = document.querySelector('.tns-liveregion');
-nav.style.display = 'none';
+if (slider.isOn) {
+  const outerWrapper = document.querySelector('.tns-ovh');
+  outerWrapper.style.overflow = 'visible';
+}
 
-console.log(slider.getInfo(), nav);
+slider.events.on('touchEnd', (evt) => {
+  const slideItems = evt.slideItems;
+  const slideItemsArr = Array.from(slideItems);
+
+  console.log(evt);
+
+  const activeSlideItems = slideItemsArr.filter(elem => {
+    return elem.classList.contains('tns-slide-active');
+  });
+
+  const activeSlide = activeSlideItems[1];
+  const activeSlideImage = activeSlide.querySelector('img');
+
+  activeSlideItems.forEach((elem, i) => {
+    if (i === 1) {
+      activeSlide.classList.add('active-height');
+      activeSlideImage.classList.add('active-height');
+    }
+
+    activeSlide.classList.remove('active-height');
+    activeSlideImage.classList.remove('active-height');
+  });
+
+  console.log(activeSlideItems);
+});
+
+console.log(slider);
