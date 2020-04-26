07ce26c79671d45ae67d8ba3a926ecd9b10dd35f
Anton_Kuzmitsky <tat7106@gmail.com>
2020-04-26 18:42:04 +0600

    update slider

--- src/js/main/slider.js
+++ src/js/main/slider.js
@@ -2,51 +2,48 @@ import { tns } from '../../../node_modules/tiny-slider/src/tiny-slider';
 
 const slider = tns({
   container: '.programme___slider',
-  // items: 2,
-  // slideBy: 1,
-  // center: true,
-  // fixedWidth: 260,
-  // gutter: 10,
-  controls: false,
-  nav: false,
-
   items: 1,
   slideBy: 1,
-  center: true,
-  // autoWidth: true,
   edgePadding: 15,
   gutter: 16,
+  center: true,
+  controls: false,
+  nav: false,
+  mouseDrag: true,
 });
 
 if (slider.isOn) {
+  const sliderOptions = slider.getInfo();
   const outerWrapper = document.querySelector('.tns-ovh');
+  const slideItems = sliderOptions.slideItems;
+  const slideItemsArr = Array.from(slideItems);
+  const activeSlideIndex = sliderOptions.index;
+  const activeSlide = slideItemsArr[activeSlideIndex];
+  const activeSlideImage = activeSlide.querySelector('img');
+
   outerWrapper.style.overflow = 'visible';
+  activeSlide.classList.add('active-height');
+  activeSlideImage.classList.add('active-height');
 }
 
-slider.events.on('touchEnd', (evt) => {
+slider.events.on('indexChanged', (evt) => {
   const slideItems = evt.slideItems;
   const slideItemsArr = Array.from(slideItems);
 
-  console.log(evt);
+  slideItemsArr.forEach(elem => {
+    const slideImage = elem.querySelector('img');
 
-  const activeSlideItems = slideItemsArr.filter(elem => {
-    return elem.classList.contains('tns-slide-active');
-  });
+    elem.classList.remove('active-height');
+    slideImage.classList.remove('active-height');
 
-  const activeSlide = activeSlideItems[1];
-  const activeSlideImage = activeSlide.querySelector('img');
+    const prevElem = elem.previousElementSibling;
+    const nextElem = elem.nextElementSibling;
 
-  activeSlideItems.forEach((elem, i) => {
-    if (i === 1) {
-      activeSlide.classList.add('active-height');
-      activeSlideImage.classList.add('active-height');
+    if (prevElem && nextElem) {
+      if (prevElem.classList.contains('tns-slide-active') && nextElem.classList.contains('tns-slide-active')) {
+        elem.classList.add('active-height');
+        slideImage.classList.add('active-height');
+      }
     }
-
-    activeSlide.classList.remove('active-height');
-    activeSlideImage.classList.remove('active-height');
   });
-
-  console.log(activeSlideItems);
 });
-
-console.log(slider);
